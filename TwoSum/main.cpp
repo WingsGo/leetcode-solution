e5919fe2b90e20ee137882fa2de1b5294f7aac46
WingC <1018957763@qq.com>
2018-01-23 18:16:56 +0800

    added TwoSum solution

---
+++ TwoSum/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(auto it = nums.begin(); it != nums.end(); it++){
+            for(int idx = it - nums.begin() + 1; idx < nums.size(); idx++){
+                if(*it + nums.at(idx) == target){
+                    return vector<int>{it - nums.begin(), idx};
+                }
+            }
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    vector<int> test{-1,-2,-3,-4,-5};
+    Solution s;
+    auto result = s.twoSum(test, -8);
+    cout << result.at(0) << result.at(1) << endl;
+    getchar();
+    return 0;
+}
