f4c3a257e44197c8fdee1481a40647cf2bca0721
WingC <1018957763@qq.com>
2018-01-31 15:13:19 +0800

    added Max Consecutive Ones

---
+++ Array/Max_Consecutive_Ones/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int max = 0; int times = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]&1){
+                max = max>++times?max:++times;
+            }
+            else{
+                times = 0;
+            }
+        }
+        return max;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    vector<int> a{0,1,0,1,1,1,0,0,1};
+    Solution s;
+    cout << s.findMaxConsecutiveOnes(a);
+    return 0;
+}
