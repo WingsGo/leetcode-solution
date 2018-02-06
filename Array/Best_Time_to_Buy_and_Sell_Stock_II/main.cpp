a4abbde60178856fb438161347b0ccc5cc662b9a
WingC <1018957763@qq.com>
2018-02-06 09:48:03 +0800

    added best time to buy and sell stock II

---
+++ Array/Best_Time_to_Buy_and_Sell_Stock_II/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int siz = prices.size();
+        if (siz == 0) {
+            return 0;
+        }
+        else if (siz == 1) {
+            return prices[0];
+        }
+        else {
+            int res = 0;
+            for(int i=0; i<siz - 1; ++i) {
+                if (prices[i] < prices[i+1]) {
+                    res += prices[i+1] - prices[i];
+                }
+            }
+            return res;
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+
+{
+    vector<int> a{1};
+    Solution s;
+    cout << s.maxProfit(a);
+    return 0;
+}
