71f12368bf2453aad6901a2fc9adeb016544f46d
WingC <1018957763@qq.com>
2018-03-15 20:14:58 +0800

    added remove from array

---
+++ Array/RemoveElement/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = 0;
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i] == val)
+                continue;
+            nums[j] = nums[i];
+            j++;
+        }
+        return j;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution s;
+    vector<int> a = {3, 2, 2, 3};
+    s.removeElement(a, 2);
+    for(vector<int>::iterator it=a.begin(); it!=a.end(); it++)
+        cout << *it;
+}
---
+++ Array/Remove_Duplicates_from_Sorted_Array/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 2)
+            return nums.size();
+        int j = 0;
+        for(int i=1; i<nums.size(); i++) {
+            if (nums[i] == nums[j])
+                continue;
+            nums[++j] = nums[i];
+        }
+        return j + 1;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    vector<int> a = {1, 1, 2};
+    Solution s;
+    s.removeDuplicates(a);
+    cout << "Hello World!" << endl;
+    return 0;
+}
