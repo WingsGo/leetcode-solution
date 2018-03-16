0675949dfffaa3abf02f92b01aca9e73e21afc92
WingC <1018957763@qq.com>
2018-03-16 11:27:59 +0800

    added plus one

---
+++ Array/Plus_One/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int add_num = 1;
+        for (int i=digits.size() - 1; i>=0; i--) {
+            int res = digits[i] + add_num;
+            add_num = res / 10;
+            digits[i] = res % 10;
+        }
+        if (add_num == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
