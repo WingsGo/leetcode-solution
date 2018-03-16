dc4dc84a267060f51a7ca78696c01b17a23748c7
WingC <1018957763@qq.com>
2018-03-16 11:28:16 +0800

    added remove duplicates II

---
+++ Array/Remove_Duplicates_from_Sorted_Array_II/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums) {
+        if (nums.size() < 2)
+            return nums.size();
+        int j = 0;
+        int times = 0;
+        for(int i=1; i<nums.size(); i++) {
+            if (nums[i] == nums[j]) {
+                times++;
+                if (times < 2)
+                    nums[++j] = nums[i];
+                else
+                    continue;
+            }
+            else {
+                nums[++j] = nums[i];
+                times = 0;
+            }
+        }
+        return j+1;
+    }
+};
